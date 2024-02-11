783a462d2f171caf5cf9c170f5ed217dc912939c
Ando0910 <e.euskadi2@gmail.com>
2024-02-11 17:06:20 +0900

    lesson08完了

---
+++ lesson08.php
@@ -0,0 +1,10 @@
+﻿<?php
+$countries = [
+    "日本" => "東京",
+    "アメリカ" => "ワシントン",
+    "イギリス" => "ロンドン",
+    "フランス" => "パリ"
+];
+foreach ($countries as $key => $country) {
+    echo "{$key}". "の首都は". "{$country}". "です。"."\n";
+}
