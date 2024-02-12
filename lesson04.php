e8f090e1ed1bd0df4a8a4a79f3e251085e8e776d
Ando0910 <e.euskadi2@gmail.com>
2024-02-12 22:52:42 +0900

    lesson04 完了

--- lesson04.php
+++ lesson04.php
@@ -1,8 +1,14 @@
 ﻿<?php
-$colors = [
-    "red",
-    "blue",
-    "yellow",
-];
+for ($i = 1; $i <= 4; $i++) {
+    for ($j = 1; $j <= 4 - $i; $j++) {
+        echo "*";
+    }
+    for ($j = 1; $j <= $i; $j++) {
+        echo $j;
+    }
+    for ($j = $i - 1; $j >= 1; $j--) {
+        echo $j;
+    }
+    echo "\n";
+};
 
-echo "{$colors[1]}". "\n";
