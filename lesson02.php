4bd4761d4af99e07e2ebe05b4bc92b92a7946419
Ando0910 <e.euskadi2@gmail.com>
2024-02-12 20:02:21 +0900

    commit test

--- lesson02.php
+++ lesson02.php
@@ -1,10 +1,1 @@
 ﻿<?php
-$apple_val = "100";
-$apple_que = "1";
-$grape_val = "200";
-$grape_que = "2";
-$sum = ($apple_val * $apple_que) + ($grape_val * $grape_que);
-
-echo "{$apple_val}円のリンゴを{$apple_que}個"."\n";
-echo "{$grape_val}円のグレープを{$grape_que}個"."\n";
-echo "合計は{$sum}円です" . "\n";
