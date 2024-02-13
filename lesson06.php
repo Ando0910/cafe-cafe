32c7661e82b49d97c3497785f833395b6c17daec
Ando0910 <e.euskadi2@gmail.com>
2024-02-13 22:18:06 +0900

    lesson06 完了

--- lesson06.php
+++ lesson06.php
@@ -1,7 +1,13 @@
 ﻿<?php
-$count = 0;
-
-while ($count < 100) {
-    $count++;
-    echo "{$count}"."\n";
+for ($i = 1; $i <= 5; $i++) {
+    for ($j = 1; $j <= 5 - $i; $j++) {
+        echo "*";
+    }
+    for ($j = 5; $j >= 6 - $i; $j--) {
+        echo $j;
+    }
+    for ($j = 7 - $i; $j <= 5 ; $j++) {
+        echo $j;
+    }
+    echo "\n";
 };
