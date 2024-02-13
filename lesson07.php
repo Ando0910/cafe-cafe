1257c302f3434cd983b9518c3bed85650d8f101a
Ando0910 <e.euskadi2@gmail.com>
2024-02-13 22:18:39 +0900

    lesson07 完了

--- lesson07.php
+++ lesson07.php
@@ -1,12 +1,25 @@
 ﻿<?php
-$countries = [
-    "日本",
-    "アメリカ",
-    "イギリス",
-    "フランス"
-];
-
-foreach ($countries as $key => $country) {
-    echo "$key".":"."$country"."\n";
+for ($i = 1; $i <= 5; $i++) {
+    for ($j = 1; $j <= 5 - $i; $j++) {
+        echo "*";
+    }
+    for ($j = 1; $j <= $i; $j++) {
+        echo $j;
+    }
+    for ($j = $i - 1; $j >= 1; $j--) {
+        echo $j;
+    }
+    echo "\n";
 };
-
+for ($i = 1; $i <= 4; $i++) {
+    for ($j = 1; $j <= $i; $j++) {
+        echo "*";
+    }
+    for ($j = 1; $j <= 5 - $i; $j++) {
+        echo $j;
+    }
+    for ($j = $j - 2; $j >= 1; $j--) {
+        echo $j;
+    }
+    echo "\n";
+};
