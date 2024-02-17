5449fd6de34af8144922e1717ce0f77f06d05d1c
Ando0910 <e.euskadi2@gmail.com>
2024-02-17 15:36:23 +0900

    lesson10完了

--- lesson10.php
+++ lesson10.php
@@ -1,20 +1,13 @@
 ﻿<?php
-$scores = [
-    10,
-    60,
-    90,
-    70,
-    50
-];
-
-foreach ($scores as $score){
-    if($score >= 80) {
-        echo "{$score}"."点は優です。"."\n";
-    } elseif($score >= 60) {
-        echo "{$score}"."点は良です。"."\n";
-    } elseif($score >= 40) {
-        echo "{$score}"."点は可です。"."\n";
-    } else {
-        echo "{$score}"."点は不可です。"."\n";
+function isLeapYear($year)
+{
+    // この関数に判定処理を記述
+    for ($i = 1980; $i <= 2080; $i++) {
+        if ((($i % 4 == 0) && !($i % 100 == 0)) || ($i % 400 == 0)) {
+            echo "{$i}" . "はうるう年です。" . "\n";
+        } else {
+            echo "{$i}" . "\n";
+        }
     }
-};
+}
+isLeapYear($i);
