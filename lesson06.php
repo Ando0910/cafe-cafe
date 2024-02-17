ae2ba4893ae8dcdc6f9bde75a088863bf9f3551d
Ando0910 <e.euskadi2@gmail.com>
2024-02-17 15:35:36 +0900

    lesson06完了

--- lesson06.php
+++ lesson06.php
@@ -6,7 +6,7 @@ for ($i = 1; $i <= 5; $i++) {
     for ($j = 5; $j >= 6 - $i; $j--) {
         echo $j;
     }
-    for ($j = 7 - $i; $j <= 5 ; $j++) {
+    for ($j = 7 - $i; $j <= 5; $j++) {
         echo $j;
     }
     echo "\n";
