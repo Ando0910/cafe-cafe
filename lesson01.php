eb4b10dcca2c1f93b33f7d05cb3e2546cf326b80
Ando0910 <e.euskadi2@gmail.com>
2024-02-17 22:47:50 +0900

    lesson01完了

--- lesson01.php
+++ lesson01.php
@@ -1,6 +1,20 @@
 <?php
 
-function fizzbuzz($i) {
+function fizzbuzz($i)
+{
     // この関数内に処理を記述
+    if (($i % 3 == 0) && ($i % 5 == 0)) {
+        echo "{$i}" . " FIzzBuzz" . "\n";
+    } elseif ($i % 5 == 0) {
+        echo "{$i}" . " Buzz" . "\n";
+    } elseif ($i % 3 == 0) {
+        echo "{$i}" . " FIzz" . "\n";
+    } else {
+        echo "{$i}" . "\n";
+    }
 }
-?>
+
+
+for ($i = 1; $i <= 100; $i++) {
+    fizzbuzz($i) . "\n";
+}
