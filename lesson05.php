fadd90e00664ade145ed4e8993ebcb55d08a90a5
Ando0910 <e.euskadi2@gmail.com>
2024-02-26 22:23:58 +0900

    lesson05完了

--- lesson05.php
+++ lesson05.php
@@ -1,284 +1,175 @@
 <?php
+// 手札
+function hand()
+{
+    $cards = [];
+    for ($i = 1; $i <= 5; $i++) {
+        if (isset($_POST["suit$i"]) && isset($_POST["number$i"])) {
+            $suit = $_POST["suit$i"];
+            $number = $_POST["number$i"];
+            $cards[] = ['suit' => $suit, 'number' => $number];
+        }
+    }
 
-// 手札テストケース
-//  ロイヤルストレートフラッシュ
-$cards = [
-    ['suit' => 'heart', 'number' => 1],
-    ['suit' => 'heart', 'number' => 10],
-    ['suit' => 'heart', 'number' => 12],
-    ['suit' => 'heart', 'number' => 13],
-    ['suit' => 'heart', 'number' => 11],
-];
-//  ロイヤルストレートフラッシュjoker
-// $cards = [
-//     ['suit' => 'heart', 'number' => 1],
-//     ['suit' => 'heart', 'number' => 10],
-//     ['suit' => 'heart', 'number' => 12],
-//     ['suit' => 'heart', 'number' => 13],
-//     ['suit' => 'joker', 'number' => 0],
-// ];
-//  ストレートフラッシュ
-// $cards = [
-//     ['suit' => 'heart', 'number' => 1],
-//     ['suit' => 'heart', 'number' => 2],
-//     ['suit' => 'heart', 'number' => 3],
-//     ['suit' => 'heart', 'number' => 4],
-//     ['suit' => 'heart', 'number' => 5],
-// ];
-//  ストレートフラッシュjoker
-// $cards = [
-//     ['suit' => 'heart', 'number' => 1],
-//     ['suit' => 'heart', 'number' => 2],
-//     ['suit' => 'joker', 'number' => 0],
-//     ['suit' => 'heart', 'number' => 4],
-//     ['suit' => 'heart', 'number' => 5],
-// ];
-//  フォーカード
-// $cards = [
-//     ['suit' => 'spade', 'number' => 1],
-//     ['suit' => 'heart', 'number' => 1],
-//     ['suit' => 'diamond', 'number' => 1],
-//     ['suit' => 'club', 'number' => 1],
-//     ['suit' => 'heart', 'number' => 13],
-// ];
-//  フォーカードjoker
-// $cards = [
-//     ['suit' => 'spade', 'number' => 1],
-//     ['suit' => 'heart', 'number' => 1],
-//     ['suit' => 'diamond', 'number' => 1],
-//     ['suit' => 'joker', 'number' => 0],
-//     ['suit' => 'heart', 'number' => 13],
-// ];
-//  フルハウス
-// $cards = [
-//     ['suit' => 'spade', 'number' => 1],
-//     ['suit' => 'heart', 'number' => 1],
-//     ['suit' => 'diamond', 'number' => 1],
-//     ['suit' => 'club', 'number' => 2],
-//     ['suit' => 'heart', 'number' => 2],
-// ];
-//  フルハウスjoker
-// $cards = [
-//     ['suit' => 'spade', 'number' => 1],
-//     ['suit' => 'heart', 'number' => 1],
-//     ['suit' => 'joker', 'number' => 0],
-//     ['suit' => 'club', 'number' => 2],
-//     ['suit' => 'heart', 'number' => 2],
-// ];
-//  フラッシュ
-// $cards = [
-//     ['suit' => 'heart', 'number' => 1],
-//     ['suit' => 'heart', 'number' => 2],
-//     ['suit' => 'heart', 'number' => 6],
-//     ['suit' => 'heart', 'number' => 4],
-//     ['suit' => 'heart', 'number' => 10],
-// ];
-//  フラッシュjoker
-// $cards = [
-//     ['suit' => 'heart', 'number' => 1],
-//     ['suit' => 'heart', 'number' => 8],
-//     ['suit' => 'heart', 'number' => 6],
-//     ['suit' => 'heart', 'number' => 4],
-//     ['suit' => 'joker', 'number' => 0],
-// ];
-//  ストレート
-// $cards = [
-//     ['suit' => 'spade', 'number' => 1],
-//     ['suit' => 'heart', 'number' => 2],
-//     ['suit' => 'diamond', 'number' => 3],
-//     ['suit' => 'club', 'number' => 4],
-//     ['suit' => 'heart', 'number' => 5],
-// ];
-//  ストレートjoker
-// $cards = [
-//     ['suit' => 'spade', 'number' => 1],
-//     ['suit' => 'heart', 'number' => 2],
-//     ['suit' => 'diamond', 'number' => 3],
-//     ['suit' => 'joker', 'number' => 0],
-//     ['suit' => 'heart', 'number' => 5],
-// ];
-//  スリーカード
-// $cards = [
-//     ['suit' => 'spade', 'number' => 1],
-//     ['suit' => 'heart', 'number' => 1],
-//     ['suit' => 'club', 'number' => 1],
-//     ['suit' => 'club', 'number' => 2],
-//     ['suit' => 'heart', 'number' => 4],
-// ];
-//  スリーカードjoker
-// $cards = [
-//     ['suit' => 'spade', 'number' => 1],
-//     ['suit' => 'heart', 'number' => 1],
-//     ['suit' => 'joker', 'number' => 0],
-//     ['suit' => 'club', 'number' => 2],
-//     ['suit' => 'heart', 'number' => 4],
-// ];
-//  ツーペア
-// $cards = [
-//     ['suit' => 'spade', 'number' => 6],
-//     ['suit' => 'heart', 'number' => 6],
-//     ['suit' => 'diamond', 'number' => 8],
-//     ['suit' => 'club', 'number' => 2],
-//     ['suit' => 'heart', 'number' => 2],
-// ];
-//  ツーペアjoker(判定ではスリーカード)
-// $cards = [
-//     ['suit' => 'spade', 'number' => 6],
-//     ['suit' => 'heart', 'number' => 6],
-//     ['suit' => 'diamond', 'number' => 8],
-//     ['suit' => 'joker', 'number' => 0],
-//     ['suit' => 'heart', 'number' => 2],
-// ];
-//  ワンペア
-// $cards = [
-//     ['suit' => 'spade', 'number' => 6],
-//     ['suit' => 'heart', 'number' => 6],
-//     ['suit' => 'diamond', 'number' => 8],
-//     ['suit' => 'club', 'number' => 2],
-//     ['suit' => 'heart', 'number' => 10],
-// ];
-//  ワンペアjoker
-// $cards = [
-//     ['suit' => 'spade', 'number' => 6],
-//     ['suit' => 'joker', 'number' => 0],
-//     ['suit' => 'diamond', 'number' => 8],
-//     ['suit' => 'club', 'number' => 2],
-//     ['suit' => 'heart', 'number' => 10],
-// ];
-//  役なし
-// $cards = [
-//     ['suit' => 'heart', 'number' => 1],
-//     ['suit' => 'spade', 'number' => 5],
-//     ['suit' => 'club', 'number' => 10],
-//     ['suit' => 'diamond', 'number' => 13],
-//     ['suit' => 'heart', 'number' => 3],
-// ];
+    return $cards;
+}
 
+// 判定
 function judge($cards)
 {
     // この関数内に処理を記述
+    // カードの不正チェック
     $cover = array_unique($cards, SORT_REGULAR);
     $unique = count($cover);
 
-    $suits = ['heart', 'spade', 'diamond', 'club', 'joker'];
+    $suits = ['heart', 'spade', 'diamond', 'club'];
 
-    echo "手札は\n";
     foreach ($cards as $card) {
-        echo $card['suit'] . $card['number'] . "  ";
-        if ($card['number'] > 13 || $card['number'] < 0) {
-            return "手札が不正";
-        } elseif (!is_int($card['number'])) {
+        $number = filter_var($card['number'], FILTER_VALIDATE_INT);
+        if ($number === false || $card['number'] > 13 || $card['number'] < 1) {
             return "\n" . "手札は不正です";
         } elseif (!in_array($card['suit'], $suits)) {
-            return "\n" . "手札は不正です";
-        } elseif ($card['suit'] == "joker" && $card['number'] != 0) {
-            return "\n" . "手札は不正です";
-        } elseif ($card['suit'] !== "joker" && $card['number'] == 0) {
-            return "\n" . "手札は不正です";
+            return "手札は不正です";
         } elseif ($unique < 5)
             return "\n" . "手札は不正です";
     }
-    echo "\n";
 
-    // カードの並び替え
-    $sort = array_column($cards, 'number');
-    sort($sort);
+    // 役判定
+    $numbers = array_column($cards, 'number');
+    sort($numbers);
     $suit_array = array_column($cards, 'suit');
-    $countNum = array_count_values($sort);
+    $countNum = array_count_values($numbers);
 
-    $search = 'joker';
-    $jokerIncluded = in_array($search, $suit_array);
-
-    $hasJoker = false;
-    $numbers = array();
-    $suits = array();
-    foreach ($cards as $card) {
-        if ($card['suit'] === 'joker') {
-            $hasJoker = true;
-        } else {
-            $numbers[] = $card['number'];
-            $suits[] = $card['suit'];
+    $Straight = true;
+    for ($i = 0; $i < count($numbers) - 1; $i++) {
+        if ($numbers[$i] + 1 != $numbers[$i + 1]) {
+            $Straight = false;
+            break;
         }
     }
 
-    $uniqueNumbers = array_unique($numbers);
-    sort($uniqueNumbers);
-    $uniqueSuits = array_unique($suits);
-
-
-    $isStraight = false;
-    if (count($uniqueNumbers) === 5 || ($hasJoker && count($uniqueNumbers) === 4)) {
-        if ($hasJoker) {
-            $isStraight = ($uniqueNumbers[3] - $uniqueNumbers[0] <= 4) || ($uniqueNumbers[2] - $uniqueNumbers[0] <= 3);
-        } else {
-            $isStraight = ($uniqueNumbers[4] - $uniqueNumbers[0] === 4);
-        }
-    }
-
-    $isRoyalStraightFlush = false;
-    $royalNumbers = [1, 10, 11, 12, 13];
-    $diff = array_diff($royalNumbers, $uniqueNumbers);
-
-    if (empty($diff) && count($uniqueSuits) === 1) {
-        $isRoyalStraightFlush = true;
-    } elseif (count($diff) === 1 && $hasJoker && count($uniqueSuits) <= 1) {
-        $isRoyalStraightFlush = true;
-    }
-
-    if ($isRoyalStraightFlush) {
+    if (($numbers == [1, 10, 11, 12, 13]) && (count(array_unique($suit_array)) === 1)) {
         $result = 1;
-    } elseif (count(array_unique($suit_array)) === 1 && $isStraight || count(array_unique($suit_array)) === 2 && $isStraight) {
+    } elseif (count(array_unique($suit_array)) === 1 && $Straight) {
         $result = 2;
-    } elseif (in_array(4, $countNum) || in_array(3, $countNum) && $jokerIncluded) {
+    } elseif (in_array(4, $countNum)) {
         $result = 3;
-    } elseif (in_array(3, $countNum) && in_array(2, $countNum) || (in_array(2, $countNum) && count(array_keys($countNum, 2)) == 2) && $jokerIncluded) {
+    } elseif (in_array(3, $countNum) && in_array(2, $countNum)) {
         $result = 4;
-    } elseif ((count(array_unique($suit_array)) === 1) || (count(array_unique($suit_array)) === 2) && ($jokerIncluded)) {
+    } elseif (count(array_unique($suit_array)) === 1) {
         $result = 5;
-    } elseif ($isStraight) {
+    } elseif ($Straight) {
         $result = 6;
-    } elseif (in_array(3, $countNum) || ((in_array(2, $countNum) && $jokerIncluded))) {
+    } elseif (in_array(3, $countNum)) {
         $result = 7;
-    } elseif ((in_array(2, $countNum) && count(array_keys($countNum, 2)) == 2) || ((in_array(2, $countNum) && $jokerIncluded))) {
+    } elseif (in_array(2, $countNum) && count(array_keys($countNum, 2)) == 2) {
         $result = 8;
     } elseif (in_array(2, $countNum)) {
-        $result = 9;
-    } elseif ($jokerIncluded) {
         $result = 9;
     } else {
         $result = 0;
     }
+
     switch ($result) {
         case 1:
-            return "役はロイヤルストレートフラッシュです";
+            return "ロイヤルストレートフラッシュです";
             break;
         case 2:
-            return "役はストレートフラッシュです";
+            return "ストレートフラッシュです";
             break;
         case 3:
-            return "役はフォーカードです";
+            return "フォーカードです";
             break;
         case 4:
-            return "役はフルハウスです";
+            return "フルハウスです";
             break;
         case 5:
-            return "役はフラッシュです";
+            return "フラッシュです";
             break;
         case 6:
-            return "役はストレートです";
+            return "ストレートです";
             break;
         case 7:
-            return "役はスリーカードです";
+            return "スリーカードです";
             break;
         case 8:
-            return "役はツーペアです";
+            return "ツーペアです";
             break;
         case 9:
-            return "役はワンペアです";
+            return "ワンペアです";
             break;
         case 0:
-            return "役はなしです";
+            return "なしです";
             break;
     }
 }
-echo judge($cards) . "\n";
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" style="text/css" href="./css/style.css">
+    <title>ポーカー役判定</title>
+</head>
+
+<body>
+    <form action="#" method="POST" name="formtype">
+        <section>
+            <div class="flex">
+                <?php for ($i = 1; $i <= 5; $i++) { ?>
+                    <div class="card">
+                        <p><?php echo $i . ":" ?>
+                            <select name="<?php echo "suit" . $i ?>" class="suit">
+                                <option value=""></option>
+                                <option value="spade">spade</option>
+                                <option value="diamond">diamond</option>
+                                <option value="heart">heart</option>
+                                <option value="club">club</option>
+                            </select>
+                            <select name="<?php echo "number" . $i ?>">
+                                <option value=""></option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                                <option value="13">13</option>
+                            </select>
+                    </div>
+                <?php } ?>
+                <button type="submit" class="button1" name="submit">判定</button>
+            </div>
+            <div>
+                <!-- 「hand」関数を使用してセレクトボックスで入力した手札を戻り値で取得し、ブラウザー上で表示する。 -->
+                <!-- 引数の仕様有無は各自の判断に任せるとする。-->
+                <?php if (!empty($_POST)) : ?>
+                    <p>手札は
+                        <?php foreach (hand() as $card) : ?>
+                            <?= htmlspecialchars($card['suit']) . " " . htmlspecialchars($card['number']) . "　"; ?>
+                        <?php endforeach; ?>
+                    </p>
+                <?php endif; ?>
+            </div>
+            <div>
+                <!-- 「judge」関数を使用してセレクトボックスで入力した手札から役を戻り値で取得し、ブラウザー上で表示する。 -->
+                <!-- 引数の仕様有無は各自の判断に任せるとする。-->
+                <p>役は<?php
+                        $cards = hand();
+                        echo judge($cards) ?>
+                </p>
+            </div>
+        </section>
+    </form>
+</body>
+
+</html>
