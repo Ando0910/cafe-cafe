2f681e9a084bd5a982f6a15958507a8923a1acfa
Ando0910 <e.euskadi2@gmail.com>
2024-03-12 00:20:16 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/ress.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>4-5 cafe-cafe</title>
+</head>
+
+<body>
+    <div class="covid">
+        <div class="covid-area">
+            <a href="">
+                <p>新型コロナウイルスに対する取り組みの最新情報をご案内</p>
+            </a>
+        </div>
+    </div>
+    <?php require 'header.php'; ?>
+    <div class="main-view">
+        <div class="main-view__area">
+            <img src="/img/eyecatch.jpg" alt="">
+        </div>
+        <div class="filter"></div>
+        <div class="main-copy">
+            <p>あなたの<br>好きな空間を作る。</p>
+        </div>
+    </div>
+    <main>
+        <section class="first-sec" id="first">
+            <div class="contents-height">
+                <div class="contents-width">
+                    <div class="first-contents-area">
+                        <div class="first-contents-list" id="first-contents__list">
+                            <div class="first-contents__item">
+                                <div class=first-contents__pic>
+                                    <img src="/img/cafe1.jpg" alt="">
+                                </div>
+                                <div class=first-contents__disc>
+                                    <p>東京<br>車で15分</p>
+                                </div>
+                            </div>
+                            <div class="first-contents__item">
+                                <div class=first-contents__pic>
+                                    <img src="/img/cafe2.jpg" alt="">
+                                </div>
+                                <div class=first-contents__disc>
+                                    <p>神奈川<br>車で30分</p>
+                                </div>
+                            </div>
+                            <div class="first-contents__item">
+                                <div class=first-contents__pic>
+                                    <img src="/img/cafe3.jpg" alt="">
+                                </div>
+                                <div class=first-contents__disc>
+                                    <p>愛知<br>車で1時間</p>
+                                </div>
+                            </div>
+                            <div class="first-contents__item">
+                                <div class=first-contents__pic>
+                                    <img src="/img/cafe4.jpg" alt="">
+                                </div>
+                                <div class=first-contents__disc>
+                                    <p>京都<br>車で40分</p>
+                                </div>
+                            </div>
+                            <div class="first-contents__item">
+                                <div class=first-contents__pic>
+                                    <img src="/img/cafe6.jpg" alt="">
+                                </div>
+                                <div class=first-contents__disc>
+                                    <p>岡山<br>車で1.5時間</p>
+                                </div>
+                            </div>
+                            <div class="first-contents__item">
+                                <div class=first-contents__pic>
+                                    <img src="/img/cafe6.jpg" alt="">
+                                </div>
+                                <div class=first-contents__disc>
+                                    <p>鹿児島<br>車で50分</p>
+                                </div>
+                            </div>
+                            <div class="first-contents__item">
+                                <div class=first-contents__pic>
+                                    <img src="/img/cafe7.jpg" alt="">
+                                </div>
+                                <div class=first-contents__disc>
+                                    <p>沖縄<br>車で2時間
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="second-sec">
+            <div class="contents-height">
+                <div class="contents-width">
+                    <h2 class="sec-title">好きなロケーションを選ぼう</h2>
+                    <div class="second-contents__area">
+                        <ul class="second-contents__list">
+                            <li class="second-contents__item">
+                                <div class=second-contents__pic>
+                                    <img src="/img/intro1.jpg" alt="">
+                                </div>
+                                <div class=second-contents__disc>
+                                    <p>クラシック</p>
+                                </div>
+                            </li>
+                            <li class="second-contents__item">
+                                <div class=second-contents__pic>
+                                    <img src="/img/intro2.jpg" alt="">
+                                </div>
+                                <div class=second-contents__disc>
+                                    <p>バー</p>
+                                </div>
+                            </li>
+                            <li class="second-contents__item">
+                                <div class=second-contents__pic>
+                                    <img src="/img/intro3.jpg" alt="">
+                                </div>
+                                <div class=second-contents__disc>
+                                    <p>キャンプ</p>
+                                </div>
+                            </li>
+                            <li class="second-contents__item">
+                                <div class=second-contents__pic>
+                                    <img src="/img/intro4.jpg" alt="">
+                                </div>
+                                <div class=second-contents__disc>
+                                    <p>リゾート</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="third-sec">
+            <div class="contents-height">
+                <div class="contents-width">
+                    <div class="pic">
+                        <div class="third-contents__disc-area">
+                            <h2 class="sec-title">
+                                Go To Eats
+                            </h2>
+                            <div class="third-contents__disc">
+                                <p>キャンペーンを利用して、全国で食事しよう。<br>いつもと違う景色に囲まれてカラダもココロもリフレッシュ。
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="forth-sec" id="forth">
+            <div class="contents-height">
+                <div class="contents-width">
+                    <h2 class="sec-title">カフェ作りを体験しよう</h2>
+                    <div class="forth-contents__disc">
+                        <p>お店のエキスパートが案内するユニークな体験（直接対面型またはオンライン）。</p>
+                    </div>
+                    <div class="forth-contents-area">
+                        <ul class="forth-contents-list">
+                            <li class="forth-contents__item">
+                                <div class=forth-contents__pic>
+                                    <img src="/img/exp1.jpg" alt="">
+                                </div>
+                                <div class=forth-contents__disc>
+                                    <p><span>ジョブ体験</span><br>カフェカウンターを体験しよう。</p>
+                                </div>
+                            </li>
+                            <li class="forth-contents__item">
+                                <div class="forth-contents__pic">
+                                    <img src="/img/exp2.jpg" alt="">
+                                </div>
+                                <div class=forth-contents__disc>
+                                    <p><span>レシピ体験</span><br>美味しいレシピを考えてみよう。</p>
+                                </div>
+                            </li>
+                            <li class="forth-contents__item">
+                                <div class=forth-contents__pic>
+                                    <img src="/img/exp3.jpg" alt="">
+                                </div>
+                                <div class=forth-contents__disc>
+                                    <p><span>プロモーション体験</span><br>お店の宣伝を手伝ってみよう。</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="fifth">
+            <div class="contents-height">
+                <div class="contents-width">
+                    <div class="second-contents__area">
+                        <ul class="second-contents__list">
+                            <li class="second-contents__item">
+                                <div class=second-contents__pic>
+                                    <img src="/img/host1.jpg" alt="">
+                                </div>
+                                <div class=second-contents__disc>
+                                    <p>ビジネス</p>
+                                </div>
+                            </li>
+                            <li class="second-contents__item">
+                                <div class=second-contents__pic>
+                                    <img src="/img/host2.jpg" alt="">
+                                </div>
+                                <div class=second-contents__disc>
+                                    <p>コミュニティ</p>
+                                </div>
+                            </li>
+                            <li class="second-contents__item">
+                                <div class=second-contents__pic>
+                                    <img src="/img/host3.jpg" alt="">
+                                </div>
+                                <div class=second-contents__disc>
+                                    <p>食べ歩き</p>
+                                </div>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+    <?php require 'footer.php'; ?>
+
+</body>
+
+</html>
